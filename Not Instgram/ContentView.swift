dac5d3539d0cace2dd3ed9186603ac940702e7fe
isaac <baa44422@gmail.com>
2021-06-29 10:50:52 +0800

    (V) Added some buttons

--- Not Instgram/ContentView.swift
+++ Not Instgram/ContentView.swift
@@ -10,17 +10,37 @@ import SwiftUI
 struct TextView: View {
     var body: some View {
         Text("Welcome to Insta gram??")
-            .font(Font.custom("Helvetica Neue", size: 24))
+            .font(Font.custom("Helvetica Neue", size: 29))
             .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
             .padding()
     }
 }
 struct ContentView: View {
+    
+    @State private var username = ""
+    @State private var password = ""
+    
     var body: some View {
         VStack {
             Rectangle()
                 .fill(LinearGradient(gradient: Gradient(colors: [Color.pink, Color.red, Color.yellow]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
                 .mask(TextView())
+            
+            VStack {
+                TextField("Username", text: $username)
+                    .frame(width: 350, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .background(Color.gray)
+                    .cornerRadius(5)
+                    .padding()
+                
+                SecureField("Password", text: $password)
+                    .frame(width: 350, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .background(Color.gray)
+                    .cornerRadius(5)
+                    .padding()
+            }
+            .offset(x: 0, y: -300)
+            
         }
     }
 }
