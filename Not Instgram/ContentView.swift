ffa01939e11f786c02fda8490e93cd432fb35324
isaac <baa44422@gmail.com>
2021-06-29 10:40:19 +0800

    (V) Added a linear gradient for the heart

--- Not Instgram/ContentView.swift
+++ Not Instgram/ContentView.swift
@@ -7,10 +7,21 @@
 
 import SwiftUI
 
+struct TextView: View {
+    var body: some View {
+        Text("Welcome to Insta gram??")
+            .font(Font.custom("Helvetica Neue", size: 24))
+            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            .padding()
+    }
+}
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Rectangle()
+                .fill(LinearGradient(gradient: Gradient(colors: [Color.pink, Color.red, Color.yellow]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
+                .mask(TextView())
+        }
     }
 }
 
