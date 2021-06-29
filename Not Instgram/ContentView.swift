eb135c5f6e685d68a2032be4c7cd0defc9b20ffb
isaac <baa44422@gmail.com>
2021-06-29 10:35:14 +0800

    Initial Commit

---
+++ Not Instgram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Not Instgram
+//
+//  Created by Isaac Lee Jing Zhi on 29/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Not Instgram/Not_InstgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  Not_InstgramApp.swift
+//  Not Instgram
+//
+//  Created by Isaac Lee Jing Zhi on 29/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Not_InstgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
